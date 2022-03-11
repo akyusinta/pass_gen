00975a08f3d41aeba29a306844158ced12fff5a7
sinta <sinta.if@gmail.com>
2022-03-12 06:38:28 +0700

    djangotest

---
+++ weather/urls.py
@@ -0,0 +1,9 @@
+
+from unicodedata import name
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('delete/<city_name>', views.delete_city, name='delete_city')
+]
